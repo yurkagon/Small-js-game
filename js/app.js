786e1b91cfebad95afe2ae1f9b333adf11440239
Yuragon <owngameplay@gmail.com>
2017-12-13 12:50:56 +0200

    jQuery support

--- js/app.js
+++ js/app.js
@@ -1,87 +1,90 @@
-var playerSpeed = 2;
-var playerSize = 100;
+$(window).ready(function(){
+  var playerSpeed = 2;
+  var playerSize = 100;
 
-var player = document.querySelector('#player');
+  var player = $('#player');
 
-//setting player size
-player.style.height = playerSize + 'px';
-player.style.width = playerSize + 'px';
-//setting the player on the center of page
-player.style.top = (screen.height/2 - playerSize) + 'px';
-player.style.left = (screen.width/2 - playerSize) + 'px';
+  //setting player size
+  player.css('height', playerSize + 'px');
+  player.css('width', playerSize + 'px');
+  //setting the player on the center of the page
+  player.css('top', (screen.height/2 - playerSize) + 'px');
+  player.css('left', (screen.width/2 - playerSize) + 'px');
 
 
 
-//control
-var toUp, toDown, toLeft, toRight;
-toUp=toDown=toLeft=toRight = false;
+  //control
+  var toUp, toDown, toLeft, toRight;
+  toUp=toDown=toLeft=toRight = false;
 
-document.body.onkeydown = function (e) {
-  if (e.keyCode == 37 || e.keyCode == 65 ) toLeft = true;
-  if (e.keyCode == 39 || e.keyCode == 68 ) toRight = true;
-  if (e.keyCode == 38 || e.keyCode == 87 ) toUp = true;
-  if (e.keyCode == 40 || e.keyCode == 83 ) toDown = true; 
-  console.log(e.keyCode);
-}
-document.body.onkeyup = function (e) {
-  if (e.keyCode == 37 || e.keyCode == 65 ) toLeft = false;
-  if (e.keyCode == 39 || e.keyCode == 68 ) toRight = false;
-  if (e.keyCode == 38 || e.keyCode == 87 ) toUp = false;
-  if (e.keyCode == 40 || e.keyCode == 83 ) toDown = false; 
-}
+  $('body').keydown(function (e) {
+    if (e.keyCode == 37 || e.keyCode == 65 ) toLeft = true;
+    if (e.keyCode == 39 || e.keyCode == 68 ) toRight = true;
+    if (e.keyCode == 38 || e.keyCode == 87 ) toUp = true;
+    if (e.keyCode == 40 || e.keyCode == 83 ) toDown = true; 
+    console.log(e.keyCode);
+  });
+  $('body').keyup(function (e) {
+    if (e.keyCode == 37 || e.keyCode == 65 ) toLeft = false;
+    if (e.keyCode == 39 || e.keyCode == 68 ) toRight = false;
+    if (e.keyCode == 38 || e.keyCode == 87 ) toUp = false;
+    if (e.keyCode == 40 || e.keyCode == 83 ) toDown = false; 
+  });
 
-//movement
-setInterval(function () {
+  //movement
+  setInterval(function () {
 
-  //left
-  if( parseInt(player.style.left) > 0 ){ //limit left
-      if (toLeft) {
-        player.style.left = (parseInt(player.style.left) - playerSpeed) + 'px';
+    //left
+    if( parseInt(player.css('left')) > 0 ){ //limit left
+        if (toLeft) {
+          player.css('left', (parseInt(player.css('left')) - playerSpeed) + 'px');
+        }
+    }
+   
+    //right
+    if( parseInt(player.css('left')) < screen.width - 2*playerSize ){  //limit right
+     if (toRight) {
+          player.css('left', (parseInt(player.css('left')) + playerSpeed) + 'px');
       }
-  }
- 
-  //right
-  if( parseInt(player.style.left) < screen.width - 2*playerSize ){  //limit right
-   if (toRight) {
-        player.style.left = (parseInt(player.style.left) + playerSpeed) + 'px';
     }
-  }
 
-  //up
-  if( parseInt(player.style.top) > 0 ){ //limit top
-    if (toUp) {
-      player.style.top = (parseInt(player.style.top) - playerSpeed) + 'px';
+    //up
+    if( parseInt(player.css('top')) > 0 ){ //limit top
+      if (toUp) {
+        player.css('top', (parseInt(player.css('top')) - playerSpeed) + 'px');
+      }
     }
-  }
-  
-  //down
-  if( parseInt(player.style.top) < screen.height - 2*playerSize ){ //limit bottom
-    if (toDown) {
-      player.style.top = (parseInt(player.style.top) + playerSpeed) + 'px';
+    
+    //down
+    if( parseInt(player.css('top')) < screen.height - 2*playerSize ){ //limit bottom
+      if (toDown) {
+        player.css('top', (parseInt(player.css('top')) + playerSpeed) + 'px');
+      }
     }
-  }
-},10);
+  },10);
 
 
-//rotation
-var mouseX,mouseY;
-document.getElementById('cont').onmousemove = function(e){
-  mouseX = e.pageX;
-  mouseY = e.pageY;
-}
-setInterval(function () {
+  //rotation
+  var mouseX,mouseY;
+  $('#cont').mousemove(function(e){
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+  });
+  setInterval(function () {
 
-  //center of player
-  var centerX = parseInt(player.style.left) + playerSize/2;
-  var centerY = parseInt(player.style.top) + playerSize/2; 
+    //center of player
+    var centerX = parseInt(player.css('left')) + playerSize/2;
+    var centerY = parseInt(player.css('top')) + playerSize/2; 
 
-  var dx = mouseX - centerX;
-  var dy = mouseY - centerY;
+    var dx = mouseX - centerX;
+    var dy = mouseY - centerY;
 
-  //Math.atan2(dy, dx) returns Radians, 180/Math.PI makes it as Degree
-  var degree = Math.atan2(dy, dx) * 180/Math.PI;
+    //Math.atan2(dy, dx) returns Radians, 180/Math.PI makes it as Degree
+    var degree = Math.atan2(dy, dx) * 180/Math.PI;
 
-  player.style.transform = 'rotate(' + degree + 'deg)';
-},10);
+    player.css('transform', 'rotate(' + degree + 'deg)');
+  },10);
+
+});
 
 
